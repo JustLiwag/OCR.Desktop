d076ff48ee89742286bfc4ff32fe1ab36d12f3bb
jeliwag <jeliwag@MID-JELIWAG>
2026-02-11 16:52:40 +0800

    Add project files.

---
+++ OcrApp.Designer.cs
@@ -0,0 +1,210 @@
+﻿using System.Windows.Forms;
+
+namespace OCR.Desktop
+{
+    partial class OcrApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OcrApp));
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.btnSelectPdf = new System.Windows.Forms.Button();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flpUploadPreview = new System.Windows.Forms.FlowLayoutPanel();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.flpOCRPreview = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(396, 309);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 30);
+            this.btnUpload.TabIndex = 6;
+            this.btnUpload.Text = "OCR PDF";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // btnSelectPdf
+            // 
+            this.btnSelectPdf.Location = new System.Drawing.Point(396, 273);
+            this.btnSelectPdf.Name = "btnSelectPdf";
+            this.btnSelectPdf.Size = new System.Drawing.Size(75, 30);
+            this.btnSelectPdf.TabIndex = 9;
+            this.btnSelectPdf.Text = "Select PDF";
+            this.btnSelectPdf.UseVisualStyleBackColor = true;
+            this.btnSelectPdf.Click += new System.EventHandler(this.btnSelectPdf_Click);
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFile.Location = new System.Drawing.Point(137, 472);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(100, 16);
+            this.lblFile.TabIndex = 10;
+            this.lblFile.Text = "No file selected";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(546, 465);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(265, 23);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(137, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Upload Preview";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // flpUploadPreview
+            // 
+            this.flpUploadPreview.Location = new System.Drawing.Point(59, 78);
+            this.flpUploadPreview.Name = "flpUploadPreview";
+            this.flpUploadPreview.Size = new System.Drawing.Size(265, 381);
+            this.flpUploadPreview.TabIndex = 12;
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(345, 91);
+            this.searchBox.Multiline = true;
+            this.searchBox.Name = "searchBox";
+            this.searchBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.searchBox.Size = new System.Drawing.Size(174, 20);
+            this.searchBox.TabIndex = 7;
+            // 
+            // flpOCRPreview
+            // 
+            this.flpOCRPreview.Location = new System.Drawing.Point(546, 78);
+            this.flpOCRPreview.Name = "flpOCRPreview";
+            this.flpOCRPreview.Size = new System.Drawing.Size(265, 381);
+            this.flpOCRPreview.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(643, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "OCR Preview";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(411, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 15);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Search";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(59, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(265, 381);
+            this.panel1.TabIndex = 17;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(546, 78);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(265, 381);
+            this.panel2.TabIndex = 0;
+            // 
+            // OcrApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 563);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.flpOCRPreview);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.flpUploadPreview);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.btnSelectPdf);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "OcrApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OcrApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Button btnUpload;
+        private PrintPreviewDialog printPreviewDialog1;
+        private Button btnSelectPdf;
+        private Label lblFile;
+        private ProgressBar progressBar1;
+        private Label label1;
+        private FlowLayoutPanel flpUploadPreview;
+        private TextBox searchBox;
+        private FlowLayoutPanel flpOCRPreview;
+        private Label label2;
+        private Label label3;
+        private Panel panel1;
+        private Panel panel2;
+    }
+}
+
---
+++ OcrApp.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Printing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+
+namespace OCR.Desktop
+{
+    public partial class OcrApp : Form
+    {
+
+        private string _selectedPdf;
+        private readonly HttpClient _http = new HttpClient();
+
+        public OcrApp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectPdf_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "PDF files (*.pdf)|*.pdf";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    _selectedPdf = ofd.FileName;
+                    lblFile.Text = Path.GetFileName(_selectedPdf);
+                }
+            }
+        }
+
+        private async void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(_selectedPdf))
+            {
+                MessageBox.Show("Select a PDF first.");
+                return;
+            }
+
+            btnUpload.Enabled = false;
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            searchBox.AppendText("Uploading...\r\n");
+
+            try
+            {
+                using (var form = new MultipartFormDataContent())
+                using (var fs = File.OpenRead(_selectedPdf))
+                {
+                    var fileContent = new StreamContent(fs);
+                    fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("application/pdf");
+
+                    form.Add(fileContent, "file", Path.GetFileName(_selectedPdf));
+
+                    var response = await _http.PostAsync(
+                        "https://localhost:7080/api/ocr/manual",
+                        form
+                    );
+
+                    response.EnsureSuccessStatusCode();
+
+                    var outputPdfBytes = await response.Content.ReadAsByteArrayAsync();
+
+                    var savePath = Path.Combine(
+                        Path.GetDirectoryName(_selectedPdf),
+                        Path.GetFileNameWithoutExtension(_selectedPdf) + "_OCR.pdf"
+                    );
+
+                    File.WriteAllBytes(savePath, outputPdfBytes);
+
+                    searchBox.AppendText("OCR complete!\r\nSaved: " + savePath + "\r\n");
+                    MessageBox.Show("OCR completed!");
+                }
+            }
+            catch (Exception ex)
+            {
+                searchBox.AppendText("ERROR: " + ex.Message + "\r\n");
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                progressBar1.Style = ProgressBarStyle.Blocks;
+                btnUpload.Enabled = true;
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        //private void btnUpload_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        using (OpenFileDialog ofd = new OpenFileDialog())
+        //        {
+        //            ofd.Title = "Select a file to preview";
+        //            ofd.Filter = "All Files|*.*|Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif|Text Files|*.txt";
+        //            ofd.FilterIndex = 1;
+        //            ofd.Multiselect = false;
+
+        //            if (ofd.ShowDialog() == DialogResult.OK)
+        //            {
+        //                string filePath = ofd.FileName;
+        //                lblFileName.Text = Path.GetFileName(filePath);
+
+        //                // Preview based on file type
+        //                string ext = Path.GetExtension(filePath).ToLower();
+        //                if (ext == ".jpg" || ext == ".jpeg" || ext == ".png" || ext == ".bmp" || ext == ".gif" || ext == ".webp")
+        //                {
+        //                    pictureBoxPreview.Visible = true;
+        //                    textBoxPreview.Visible = false;
+        //                    pictureBoxPreview.Image = System.Drawing.Image.FromFile(filePath);
+        //                }
+        //                else if (ext == ".txt")
+        //                {
+        //                    pictureBoxPreview.Visible = false;
+        //                    textBoxPreview.Visible = true;
+        //                    textBoxPreview.Text = File.ReadAllText(filePath);
+        //                }
+        //                else
+        //                {
+        //                    pictureBoxPreview.Visible = false;
+        //                    textBoxPreview.Visible = true;
+        //                    textBoxPreview.Text = "Preview not supported for this file type.";
+        //                }
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show("Error loading file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //    }
+        //}
+    }
+}
